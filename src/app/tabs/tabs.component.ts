f20e81339316ff6d7aed2de76b9bf2c108883f64
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-01-19 19:44:58 +0100

    passing data via property binding

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -10,4 +10,19 @@ export class TabsComponent {
     { name: 'Luke Skywalker', side: '' },
     { name: 'Darth Vader', side: '' },
   ];
+  chosenList = 'all';
+
+  onChoose(side) {
+    this.chosenList = side;
+  }
+
+  getCharacters() {
+    if (this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList;
+    });
+  }
 }
